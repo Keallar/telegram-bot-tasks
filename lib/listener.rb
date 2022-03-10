4802cd5e601898996b88e3f56d043fbc382bf4b1
Лысанский Евгений Валерьевич <elysanskiy@phoenixit.ru>
2022-03-10 15:22:17 +0300

    Fix InlineButtons

--- lib/listener.rb
+++ lib/listener.rb
@@ -4,7 +4,7 @@ require_relative 'tasks'
 require_relative 'assets/inline_button'
 
 class Listener
-  attr_accessor :message
+  attr_accessor :message, :start
   attr_reader :bot
 
   def initialize(bot, message)
@@ -15,29 +15,43 @@ class Listener
   def call 
     begin
       case @message
+      when Telegram::Bot::Types::CallbackQuery
+        case @message.data
+        when 'get_task'
+          task = Tasks.new
+          question = task.random
+          @bot.api.send_message(chat_id: @message.from.id, text: "#{question}\n}")
+          @bot.logger.info('Task was send')
+          bot.api.send_message(chat_id: @message.from.id, text: "Give task")
+
+        when 'get_motivate'
+          motivate = Motivation.new
+          value = motivate.random
+          bot.api.send_message(chat_id: @message.from.id, text: "#{value['text']}\n#{value['author']}")
+          bot.logger.info('Motivate was send')
+        else
+          bot.api.send_message(chat_id: @message.from.id, text: "Error!")
+        end
+
       when Telegram::Bot::Types::Message
         case @message.text
         when '/start'
           @bot.api.send_message(chat_id: @message.chat.id, text: "Hello, #{@message.from.first_name}!")
-          start = true
+          @start = true
           @bot.logger.info('Bot has been started working')
 
         when '/task'
           @bot.api.send_message(chat_id: @message.chat.id, text: 'Task!')
           task = Tasks.new
           question = task.random
-          @bot.api.send_message(chat_id: @message.chat.id, 
-                              text: "#{question}\n}",
-                              date: @message.date)
+          @bot.api.send_message(chat_id: @message.chat.id,text: "#{question}\n}", date: @message.date)
           @bot.logger.info('Task was send')
 
         when '/motivate'
           @bot.api.send_message(chat_id: @message.chat.id, text: 'Motivation!')
           motivate = Motivation.new
           value = motivate.random
-          @bot.api.send_message(chat_id: message.chat.id, 
-                              text: "#{value['text']}\n#{value['author']}",
-                              date: @message.date)
+          @bot.api.send_message(chat_id: message.chat.id, text: "#{value['text']}\n#{value['author']}", date: @message.date)
           @bot.logger.info('Motivate was send')
 
         when '/add_task'
@@ -48,40 +62,17 @@ class Listener
 
         when '/stop'
           @bot.api.send_message(chat_id: @message.chat.id, text: "Bye, #{message.from.first_name}!")
-          start = false
+          @start = false
           @bot.logger.info('Bot has been started working')
 
         else
-          ikb = [InlineButton::GET_TASK,
-                InlineButton::GET_MOTIVATE]
+          ikb = [InlineButton::GET_TASK, InlineButton::GET_MOTIVATE]
           markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: ikb)
           @bot.api.send_message(chat_id: @message.chat.id, text: 'Make a choice', reply_markup: markup)
         end
-
-      when Telegram::Bot::Types::CallbackQuery
-        case @message.data
-        when 'get_task'
-          task = Tasks.new
-          question = task.random
-          @bot.api.send_message(chat_id: @message.chat.id,
-                              text: "#{question}\n}",
-                              date: @message.date)
-          @bot.logger.info('Task was send')
-          bot.api.send_message(chat_id: @message.from.id, text: "Give task")
-
-        when 'get_motivate'
-          motivate = Motivation.new
-          value = motivate.random
-          bot.api.send_message(chat_id: @message.chat.id, 
-                              text: "#{value['text']}\n#{value['author']}",
-                              date: @message.date)
-          bot.logger.info('Motivate was send')
-        else
-          bot.api.send_message(chat_id: @message.from.id, text: "Error!")
-        end
       end
-    rescue => exception
-      bot.logger.info(exception.message)
+    rescue => e
+      bot.logger.info(e.message)
     end
   end
 end
